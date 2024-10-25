dda3d98e42854836140f08656c2d4be23f5ddc53
bryanagv97 <bryanagv97@gmail.com>
2024-10-24 20:44:27 -0500

    Se agrega funcionalidad para ingresar el mes de despacho

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from ftp import xm
 from ftp import liq
-# Conexion con usuario y carpeta del agente 
+from datetime import datetime
 
-user1 = xm('1017243061', 'Bwcn"1IL"Z{cSd|1', 'SFEC')
-user1.set_periodo(2024,10)  # Establece el periodo antes de conectar
+# Obtener la fecha actual
+hoy = datetime.now()
+
+# Obtener el mes y el día
+mes_actual = hoy.month
+dia_hoy = hoy.day
+año_actual = hoy.year
+
+mes_actual = mes_actual if dia_hoy>5 else mes_actual-1
+b,a = (año_actual,mes_actual) if mes_actual>1 else (año_actual-1,12)
+# Conexion con usuario y carpeta del agente 
+user1 = xm('ID_XM', 'psswd_XM', 'SFEC')
+user1.set_periodo(b,a)  # Establece el periodo antes de conectar
 user1.archivo='dspcttos' # Archivo necesario para el despacho de contratos del comercializador
 user1.version='tx2' # Version del archivo durante el mes
 user1.conexion_comercia()
