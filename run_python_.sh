1126f81b91fc5a960e9afcba1fda9d7cec856168
bryanagv97 <bryanagv97@gmail.com>
2024-10-28 00:37:02 -0500

    Agrego archivo para trasladar arvhivos apra que se copie y se cree una carpeta donde se guarden los archivos

--- run_python_.sh
+++ run_python_.sh
@@ -4,6 +4,9 @@
 # crontab -e
 # 0 8 * * * RUTAABSOLUTA/run_python_.sh
 
+# Borrar la imagen guardada
+find Ruta_absoluta_Desde_Home -type f -name "*.png" -exec rm {} \;
+
 # Cambiar al directorio correcto
 cd Ruta_absoluta_Desde_Home || exit
 
@@ -13,7 +16,7 @@ cd Ruta_absoluta_Desde_Home || exit
 
 # Ejecutar el script de Python
 python3 main.py || exit
-
+python3 trasladar.py || exit
 # Desactivar el entorno virtual (opcional)
 deactivate
 
---
+++ trasladar.py
@@ -0,0 +1,55 @@
+import os
+import shutil
+
+def listar_archivos_png(directorio):
+    # Lista para almacenar los archivos PNG
+    archivos_png = []
+
+    # Iterar sobre los archivos en el directorio
+    for nombre in os.listdir(directorio):
+        # Construir la ruta completa
+        ruta_completa = os.path.join(directorio, nombre)
+        
+        # Verificar si es un archivo y si termina con .png
+        if os.path.isfile(ruta_completa) and nombre.lower().endswith('.png'):
+            archivos_png.append(ruta_completa)  # Agregar a la lista
+
+    return archivos_png
+
+def extraer_anio_mes(ruta_archivo):
+    # Obtener el nombre del archivo
+    nombre_archivo = os.path.basename(ruta_archivo)
+    
+    # Quitar la extensión del archivo
+    nombre_sin_extension = nombre_archivo.split('.')[0]
+    
+    # Obtener las primeras dos partes separadas por '_'
+    anio_mes = '_'.join(nombre_sin_extension.split('_')[:2])
+    
+    return anio_mes
+
+# Especifica el directorio que deseas leer
+directorio = '' # Ruta donde estoy corriendo ->>>>>>>>>> Lo mas importante //// Hacerlo desde usuario del servidor
+archivos_png = listar_archivos_png(directorio)
+
+# Imprimir los archivos PNG encontrados
+directorio_destino = '' # Ruta donde quiero copiar        ->>>>>>>>>> Lo mas importante //// Hacia otro usuario del servidor que tengas los permisos de para protocolo FTP
+os.chdir(directorio_destino)
+
+
+for archivo in archivos_png:
+    directorio_creacion = extraer_anio_mes(archivo)
+    print(directorio_creacion)
+    try:
+        os.mkdir(directorio_destino+directorio_creacion)
+        print(f'Directorio "{directorio_destino+directorio_creacion}" creado exitosamente.')
+        os.chdir(directorio_destino+directorio_creacion)
+        shutil.copy(archivo,os.getcwd())
+        print(f'Directorio "{directorio_destino+directorio_creacion}" creado exitosamente.')
+    except FileExistsError:
+        print(f'El directorio "{directorio_destino+directorio_creacion}" ya existe.')
+        os.chdir(directorio_destino+directorio_creacion)
+        print(f'Ahora estás en el directorio: {os.getcwd()}')
+        shutil.copy(archivo,os.getcwd())
+    except Exception as e:
+        print(f'Error al crear el directorio: {e}')  
